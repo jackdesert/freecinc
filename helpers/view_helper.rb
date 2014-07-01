ddd3d0df911efc3dd7276749d07c30a0b6ac8edd
Jack Desert <jackdesert@gmail.com>
2014-07-01 19:24:04 +0000

    File download links use https

--- helpers/view_helper.rb
+++ helpers/view_helper.rb
@@ -1,4 +1,7 @@
 module ViewHelper
+  SERVER_NAME        =  'http://freecinc.com'
+  SECURE_SERVER_NAME = 'https://freecinc.com'
+
   def link_to(url, text)
     "<a href='#{url}'>#{text}</a>"
   end
@@ -10,6 +13,13 @@ module ViewHelper
   def stylesheets
     "<link rel='stylesheet' type='text/css' href='reset.css'>
     <link rel='stylesheet' type='text/css' href='style.css'>"
+  end
+
+  def secure_url(relative_url)
+    # Production urls should use https
+    # localhost urls should keep http
+    absolute_url = url(relative_url)
+    absolute_url.sub(SERVER_NAME, SECURE_SERVER_NAME)
   end
 
 end
