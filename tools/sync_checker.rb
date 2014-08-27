c2ef572140c88b70f74333dd14f00dee73be5bde
Ubuntu <ubuntu@ip-172-31-31-153.us-west-2.compute.internal>
2014-08-27 04:29:08 +0000

    now logging how much time each takes

--- tools/sync_checker.rb
+++ tools/sync_checker.rb
@@ -1,6 +1,7 @@
 require 'pry'
 require 'open3'
 require 'securerandom'
+require 'benchmark'
 require './ez_mail'
 
 class SyncChecker
@@ -10,12 +11,17 @@ class SyncChecker
   INDENT = '    '
 
 
-  attr_reader :time, :server
-  attr_accessor :stdout, :stderr, :exitstatus
+  attr_reader :created_at, :server
+  attr_accessor :stdout, :stderr, :exitstatus, :add_task_time, :sync_time, :email_time
 
   def initialize(server)
+    @created_at = timestamp
     @server = server
-    @time = Time.now
+    
+    # stdout and stdin are strings so they can be appended to later
+    @stdout = ''
+    @stderr = ''
+
     validate_server
   end
 
@@ -24,14 +30,26 @@ class SyncChecker
   end
 
   def up?
+    add_task
     sync
-    exitstatus == 0 && command_output.include?(SUCCESS_STRING)
+    log(time_stats)
+    exitstatus == 0 && stdout_and_stderr.include?(SUCCESS_STRING)
   end
 
   private
 
+  def timestamp
+    # This includes milliseconds
+    # 2014-08-23 15:52:15.30569822
+    Time.now.strftime "%Y-%m-%d %H:%M:%S.%7N"
+  end
+
   def taskrc
     "~/.taskrc-#{server}"
+  end
+  
+  def set_taskrc
+    "TASKRC=#{taskrc}"
   end
 
   def logfile
@@ -44,54 +62,86 @@ class SyncChecker
     end
   end
 
-  def command
-    return @command if @command
-    set_taskrc = "TASKRC=#{taskrc}"
-    @command = "#{set_taskrc} #{EXECUTABLE} add 'walk the #{SecureRandom.hex}' && #{set_taskrc} #{EXECUTABLE} sync"
+  def add_task_command
+    # memoization used to ensure that the random part remains the same for a particular instance
+    return @add_task_command if @add_task_command
+    @add_task_command = "#{set_taskrc} #{EXECUTABLE} add 'walk the #{SecureRandom.hex}'"
+  end 
+
+  def sync_command
+    # no memoization required, as no random elements exist
+    "#{set_taskrc} #{EXECUTABLE} sync"
   end
 
-  def sync
+  def run_command(command)
     Open3.popen3( command ) do |block_stdin, block_stdout, block_stderr, wait_thr|
-      self.stdout = block_stdout.read
-      self.stderr = block_stderr.read
+      self.stdout += block_stdout.read
+      self.stderr += block_stderr.read
       self.exitstatus = wait_thr.value.exitstatus
     end
     log command
-    log command_output
+    log stdout_and_stderr
+  end  
+ 
+  def add_task
+    self.add_task_time = Benchmark.measure do
+      run_command(add_task_command)
+    end
   end
 
-  def command_output
+  def sync
+    self.sync_time = Benchmark.measure do
+      run_command(sync_command)
+    end
+  end
+
+  def stdout_and_stderr
     "#{stdout} #{stderr}"
   end
 
   def log(text)
-    time = Time.now.utc
     text_formatted = text.gsub(/\n+/, "\n").gsub(/^/, INDENT)
-    text_with_time = "#{time} #{text_formatted}\n"
+    text_with_time = "#{created_at} #{text_formatted}\n"
     puts text_formatted
     File.open(logfile, 'a') {|f| f.write(text_with_time) }
   end
 
   def email_body
-    output = "taskd failed to sync at #{Time.now} on #{server}.com\n\n"
-    output += "command:\n"
-    output += command
-    output += "\n\nresponse:\n"
-    output += command_output
-    output
+    <<EOF
+taskd failed to sync at #{created_at} on #{server}.com
+commands:
+#{add_task_command}
+#{sync_command}
+response:
+#{stdout_and_stderr}
+EOF
   end
 
   def email_subject
-    "Sync Failure #{server}.com"
+    "Sync Failure #{server}.com #{created_at}"
+  end
+
+  def time_stats
+    <<EOF
+
+add_task_time:
+#{add_task_time}
+sync_time:
+#{sync_time}
+email_time:
+#{email_time}
+EOF
   end
 
 
   def notify
-    RECIPIENTS.each do |email_recipient|
-      mail = EZMail.new(email_recipient, email_subject, email_body)
-      mail.deliver
-      log("email sent to #{email_recipient}")
+    self.email_time = Benchmark.measure do
+      RECIPIENTS.each do |email_recipient|
+        mail = EZMail.new(email_recipient, email_subject, email_body)
+        mail.deliver
+      end
     end
+    log("emails sent to #{RECIPIENTS}\n#{time_stats}")
   end
 end
 
